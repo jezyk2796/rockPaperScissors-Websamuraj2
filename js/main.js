f8387a74e9264f39e6fe92e049b1ee443d9b18d3
Szymon Jeż <jezyk2796@op.pl>
2019-01-14 14:20:56 +0100

    function with drawing a picture by a computer is ready

--- js/main.js
+++ js/main.js
@@ -12,11 +12,24 @@ const game = {
 
 const pictures = [...document.querySelectorAll('.select img')];
 
+// pictures selection
 const selectPicture = (e) => {
     game.playerChoice = e.target.dataset.option;
 
     pictures.forEach(picture => picture.style.boxShadow = '');
     e.target.style.boxShadow = '0 0 0 4px purple';
-}
+};
+
+// computer selects the picture
+const computerPicture = () => {
+    return pictures[Math.floor(Math.random() * 3)].dataset.option;
+};
+
+const startGame = () => {
+    if (game.playerChoice == '') return alert('wybierz dłoń!!!!');
+
+    game.computerChoice = computerPicture();
+};
 
 pictures.forEach(picture => picture.addEventListener('click', selectPicture));
+document.querySelector('.start').addEventListener('click', startGame);
